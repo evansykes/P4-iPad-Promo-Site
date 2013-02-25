8357daa9429ae851b44da11273fac9171cd6fcd3
Evan Sykes <evan.m.sykes@gmail.com>
2013-02-24 16:36:55 -0800

    Tutorial
    *going thru tutorial
    *learning about how to set up the webapp properly
    *added a little of my own content

---
+++ assets/includes/esales_aid.js
@@ -0,0 +1,20 @@
+/*  JavaScript Document                      */
+
+$(document).ready(function() {
+  
+  setOrientationListener();
+  
+    
+});
+
+function setOrientationListener(){
+	rotationInterval = setInterval( function(){ updateOrientation(); }, 500 );
+}
+
+function updateOrientation(){
+	if($('body').width() < 1024){
+		$('.page').removeClass('landscape').addClass('portrait');
+	}else{
+		$('.page').removeClass('portrait').addClass('landscape');	
+	}
+}
