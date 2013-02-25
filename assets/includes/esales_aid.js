500cad947292d53d36dc8242a13b4e978fdebd8a
Evan Sykes <evan.m.sykes@gmail.com>
2013-02-25 09:15:50 -0800

    More tutorial
    *added a bit of javascript from the tutorial.
    *trying to figure out how to add elements
    *next? Page structures of my own...

--- assets/includes/esales_aid.js
+++ assets/includes/esales_aid.js
@@ -4,8 +4,35 @@ $(document).ready(function() {
   
   setOrientationListener();
   
+  
+  
+  $('nav a').on('click', function(){
+	  $('nav a').removeClass('selected');
+	  $(this).addClass('selected');	
+	  changePage( $(this).attr('data-file') );
+  });
     
+	$('nav a:nth-child(1)').trigger('click');
+	
+	$('.banner_logo').on('click', function(){
+		$('nav a:nth-child(1)').trigger('click');
+		});
+	
 });
+
+function changePage(fileName){
+	
+	$('.content_container').animate({opacity:0}, 500, function(){
+		$('content_loading_container').load('assets/content/'+fileName, function(){
+			$('.content_container').delay(250).animate({opacity:1}, 500);
+		});
+		if(fileName == 'index.html$v=1' ){
+			$('.page').addClass('home');
+		}else{
+			$('.page').removeClass('home');
+			}
+	});
+}
 
 function setOrientationListener(){
 	rotationInterval = setInterval( function(){ updateOrientation(); }, 500 );
